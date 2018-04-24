8cdb9ebbc6f0c997d2d1843f45c88e1696abcd61
Ian Videyko <ian.videyko@gmail.com>
2018-04-24 17:13:17 -0400

    updated layout

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   var searches = [];
 
   $(document).on("click", ".btn", function() {
+    $("#display-gifs").html("");
     var searchTerm = $(this).attr("data-search");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        searchTerm + "&api_key=dc6zaTOxFJmzC&limit=30";
+        searchTerm + "&api_key=dc6zaTOxFJmzC&limit=12";
 
     $.ajax({
       url: queryURL,
@@ -14,43 +15,35 @@ $(document).ready(function(){
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
-        var imageDiv = $("<div>");
-        // Make a paragraph tag with jQuery and store it in a variable named p.
-        var p = $("<p>").text(results[i].rating);
-        // Set the inner text of the paragraph to the rating of the image in results[i].
-        // Make an image tag with jQuery and store it in a variable named image.
         var image = $("<img>");
-        // Set the image's src to results[i]'s fixed_height.url.
         image.attr("src", results[i].images.fixed_height_still.url);
         image.attr("data-animate", results[i].images.fixed_height.url);
         image.attr("data-still", results[i].images.fixed_height_still.url);
         image.attr("data-state", "still");
         image.attr("class", "gif");
-        // Append the p variable to the imageDiv variable.
-        imageDiv.append(p);
-        // Append the image variable to the imageDiv variable.
-        imageDiv.append(image);
-        // Prepend the imageDiv variable to the element with an id of display-gifs.
-        $("#display-gifs").prepend(imageDiv);
+        $("#display-gifs").append(image);
       }
     });
   });
 
   $("#add-search").on("click", function(event){
     event.preventDefault();
+    console.log($("#search-input"));
+    if ($("#search-input").val() != ""){
+      var currentSearch = $("#search-input").val().trim();
 
-    var currentSearch = $("#search-input").val();
+      var a = $("<a>");
+      a.attr("class", "btn btn-primary");
+      a.attr("href", "#");
+      a.attr("role", "button");
+      a.attr("data-search", currentSearch);
+      a.text(currentSearch);
 
-    var a = $("<a>");
-    a.attr("class", "btn btn-default");
-    a.attr("href", "#");
-    a.attr("role", "button");
-    a.attr("data-search", currentSearch);
-    a.text(currentSearch);
-
-    searches.push(currentSearch); 
-    $("#search-buttons").append(a);
-    console.log(searches);
+      searches.push(currentSearch); 
+      $("#search-buttons").append(a);
+      console.log($("#search-input").val());
+      $("#search-input").val("");
+    }
   });
 
   // Handle animate/still feature
